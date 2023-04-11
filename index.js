12f83f50b6f3dc0e25f6bb92f430191a4e5caecf
Masab Bin Zia <94193065+MasabBinZia@users.noreply.github.com>
2023-04-11 15:01:53 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+const playGame = async () => {
+    const numGet = Math.floor(Math.random() * 5) + 1;
+    const { num1 } = await inquirer.prompt({
+        name: "num1",
+        type: "list",
+        message: chalk.blue("Guess a number between 1 and 5"),
+        choices: ["1", "2", "3", "4", "5"],
+    });
+    const userNum = parseInt(num1);
+    if (userNum === numGet) {
+        console.log(chalk.bold.green("Excellent! Your answer is correct."));
+    }
+    else {
+        console.log(chalk.bold.red(`Wrong! The correct answer was ${numGet}.`));
+    }
+    const { playAgain } = await inquirer.prompt({
+        name: "playAgain",
+        type: "list",
+        message: "Do you want to play again?",
+        choices: ["Yes", "No"],
+    });
+    if (playAgain === "Yes") {
+        await playGame();
+    }
+    else {
+        console.log(chalk.bold("Thanks for playing!"));
+    }
+};
+playGame();
